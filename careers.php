d975059e1bf6824926168240d4030aba5249fa29
Daniel870724 <sllk99@naver.com>
2019-10-08 17:24:54 +0900

    MoaPlanet Homepage Repository

---
+++ careers.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<!--[if lt IE 7]><html lang="en" class="no-js ie6"><![endif]-->
+<!--[if IE 7]><html lang="en" class="no-js ie7"><![endif]-->
+<!--[if IE 8]><html lang="en" class="no-js ie8"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="en" class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="UTF-8">
+    <title>MOA PLANET - 인재채용</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/jquery.easy-pie-chart.css">
+    <link rel="stylesheet" href="css/styles.css" title="mainStyle">
+
+    <!-- REVOLUTION BANNER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+
+    <script src="js/modernizr.custom.32033.js"></script>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+    <script type="text/JavaScript"> 
+        $(document).ready(function(e) { 
+        $('#ifrm').load(function() { 
+        $(this).height($(this).contents().find('body')[0].scrollHeight+30+"px"); 
+        //  $(this).width($(this).contents().find('body')[0].scrollWidth+"px"); 
+        }); 
+        }); 
+    </script> 
+
+    <!--[if IE]><script type="text/javascript" src="js/excanvas.compiled.js"></script><![endif]-->
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <div id="company" class="home">
+        <header>
+            <!-- Fixed navbar -->
+            <div class="navbar navbar-static-top" id="nav" role="navigation">
+                <div class="container" id="menu">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <i class="fa fa-align-justify"></i>
+                        </button>
+                        <a class="navbar-brand" href="index.html">
+                            <img src="img/MOA_logo.png" alt="">
+                        </a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="company.html">회사소개</a>
+                            </li>
+                            <li>
+                                <a href="index.html">주요사업</a>
+                            </li>
+                            <li class="active">
+                                <a href="#">인재영입</a>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">KOREAN <span class="caret"></span></a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#" style="padding: 5px; text-align: center;">Korean</a></li><!-- 
+                                    <li><a href="#" style="padding: 5px; text-align: center;">English</a></li> -->
+                                </ul>
+                            </li>
+                            <!-- <li class="social-nav visible-lg">
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-google-plus"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li> -->
+                        </ul>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                </div>
+                <!--/.container -->
+            </div>
+            <!--/.navbar -->
+        </header>
+
+
+        <section id="careers-process">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-push-2 clearfix">
+                        <div class="section-heading scrollpoint sp-effect4">
+                            <h1>채용프로세스</h1>
+                            <h4>Recruitment process</h4>
+                            <span class="divider"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="visible-lg visible-md row scrollpoint sp-effect4" style="float: none; margin: 0 auto;">
+                    <div class="process-part">
+                        <div class="carrers-img">
+                            <img src="img/careers/01.png" class="img-responsive img-center">
+                        </div>
+                        <div class="process-text">
+                            <h2>서류심사</h2>
+                        </div>
+                    </div>
+
+                    <div class="process-icon">
+                        <i class="fa fa-caret-right fa-3x"></i>
+                    </div>
+
+                    <div class="process-part">
+                        <div class="carrers-img">
+                            <img src="img/careers/02.png" class="img-responsive img-center">
+                        </div>
+                        <div class="process-text">
+                            <h2>1차 실무 담당자 면접</h2>
+                        </div>
+                    </div>
+
+                    <div class="process-icon">
+                        <i class="fa fa-caret-right fa-3x"></i>
+                    </div>
+
+                    <div class="process-part">
+                        <div class="carrers-img">
+                            <img src="img/careers/03.png" class="img-responsive img-center">
+                        </div>
+                        <div class="process-text">
+                            <h2>2차 경영진 면접</h2>
+                        </div>
+                    </div>
+
+                    <div class="process-icon">
+                        <i class="fa fa-caret-right fa-3x"></i>
+                    </div>
+
+                    <div class="process-part">
+                        <div class="carrers-img">
+                            <img src="img/careers/04.png" class="img-responsive img-center">
+                        </div>
+                        <div class="process-text">
+                            <h2>합격자 발표</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <iframe id="ifrm" width="100%" frameborder="0" marginwidth="0" scrolling="no" src="http://www.themoa.co.kr/superhero/bbs/board.php?bo_table=Recruitment"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <hr class="garo" id="maps-nav" style="margin-top: 80px "> <!-- 구분선 -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8" style="margin-bottom: 30px;">
+                        <div class="scrollpoint sp-effect5">
+                            <h2 style="font-weight: 800;">채용문의</h2>
+                            <h5><b>모아플래닛 채용관련 문의사항은 <a href="mailto:support@moaplanet.co.kr">support@moaplanet.co.kr</a>으로 연락주시기 바랍니다.</b></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer id="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-3">
+                    <a href="#">
+                         <img src="img/MOA_logo_gray.png" class="img-responsive img-center" style="padding: 18px 10px 18px 0px; float: right;">
+                    </a>
+               </div>
+
+               <div class="col-md-9 col-sm-9" style="text-align: left;">
+                    <h4>(주)모아플래닛 | 대표번호 1899-8558</h4>
+                    <p>대표이사 임승옥 |  사업자 등록 번호 852-81-00695 | 서울특별시 금천구 가산디지털 1로 168 우림라이온스밸리 C동 705호
+                    <p>COPYRIGHT &copy;2015 Moa Planet.Inc. All Rights Reserved.</p>
+               </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!--/wrap-->
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/stellar.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/jquery.easypiechart.min.js"></script>
+
+
+    <!-- jQuery REVOLUTION Slider  -->
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+    <script src="js/waypoints.min.js"></script>
+
+    <script src="js/script.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        appMaster.smoothScroll();
+        appMaster.animateScript();
+        appMaster.navSpy2();
+    });
+    </script>
+
+</body>
+
+</html>
